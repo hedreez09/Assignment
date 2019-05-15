ba4c64d8a7cd70f7b00bc2f05032ae459cbe89f2
hedreez09 <idrisolawonuola@gmail.com>
2019-05-15 11:27:29 +0100

    updaated on this value prime

--- ValuePrime.java
+++ ValuePrime.java
@@ -1,6 +1,5 @@
  import java.util.Scanner;
- //import java.lang.*;
-
+ 
  public class ValuePrime{
 
      public static void main(String... args){
@@ -9,11 +8,7 @@
 
          System.out.println("Enter the to be checked: ");
          int number = accept.nextInt();
-/*
-         if(number == 1 && number == 0){
-             System.out.println(number+" is Not s prime number");
-         }*/
-    
+
          for(int i=2; i <= (Math.sqrt(number)); i++){
              if((number % i == 0) || (number % number == 0)){
                  System.out.println(number+" not prime number");
